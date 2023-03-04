abb32b3b60e56dd6126fd220b97e9e149c8179a8
gautam <gautam.gandhi.100@gmail.com>
2023-03-04 10:59:38 +0530

    Created Custom exception classes and global exception class.

---
+++ src/main/java/in/getdreamjob/exception/GlobalResourceException.java
@@ -0,0 +1,28 @@
+package in.getdreamjob.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalResourceException {
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException resourceNotFoundException) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatusCode(HttpStatus.NOT_FOUND.value());
+        errorResponse.setMessage(resourceNotFoundException.getMessage());
+        errorResponse.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleResourceAlreadyExistsException(ResourceAlreadyExistsException alreadyExistsException) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatusCode(HttpStatus.CONFLICT.value());
+        errorResponse.setMessage(alreadyExistsException.getMessage());
+        errorResponse.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+}
---
+++ src/main/java/in/getdreamjob/exception/ResourceAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package in.getdreamjob.exception;
+
+public class ResourceAlreadyExistsException extends RuntimeException {
+    public ResourceAlreadyExistsException(String message) {
+        super(message);
+    }
+}

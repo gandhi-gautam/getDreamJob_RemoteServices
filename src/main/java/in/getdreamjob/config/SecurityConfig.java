7ef1f7ff4a2af8d07131aa5bc8f2305b26767b99
gautam <gautam.gandhi.100@gmail.com>
2023-03-09 20:02:36 +0530

    Added Global Cors

--- src/main/java/in/getdreamjob/config/SecurityConfig.java
+++ src/main/java/in/getdreamjob/config/SecurityConfig.java
@@ -3,38 +3,66 @@ package in.getdreamjob.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/api/v1/**").hasAnyRole("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/api/v1/**").hasAnyRole("ADMIN")
-                .and()
-                .httpBasic();
-    }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("arvind")
-                .password(this.passwordEncoder().encode("Asdlkj46&")).roles("ADMIN");
-        auth.inMemoryAuthentication().withUser("gautam")
-                .password(this.passwordEncoder().encode("Zxcmnb46&")).roles("NORMAL");
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .cors(Customizer.withDefaults())
+                .authorizeRequests(auth -> {
+                    auth.antMatchers(HttpMethod.POST, "/api/v1/**").hasAnyRole("ADMIN");
+                    auth.antMatchers(HttpMethod.PUT, "/api/v1/**").hasAnyRole("ADMIN");
+                })
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("arvind")
+                .password("Asdlkj46&")
+                .roles("ADMIN").build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("gautam")
+                .password("Asdlkj46")
+                .roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(admin);
     }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://onflydreamjob.com", "https://www.onflydreamjob.com",
+                "http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET"));
+        configuration.setAllowedHeaders(List.of("Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder(10);
+//    }
 }
